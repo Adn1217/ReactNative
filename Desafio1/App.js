9a4e42d34892062504e5331f37060d1f4d5a480e
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2023-04-12 20:34:13 -0500

    Mi primer commit

---
+++ Desafio1/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Primera Aplicación</Text>
+      <Text>Mi primera aplicación en React Native!!</Text>
+      <StatusBar style="auto" />
+      <Text style={styles.title}>GRACIAS</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 25
+  }
+});
