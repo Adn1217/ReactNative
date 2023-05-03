ca52060d54f6711250dbc70141b0b1711f62ba4e
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2023-05-03 11:35:06 -0500

    Se implementa lista estatica

--- Desafio2/App.js
+++ Desafio2/App.js
@@ -1,25 +1,50 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, View, Button } from 'react-native';
+import { styles } from './assets/styles/styles.js';
 
 export default function App() {
+
+  const [text, setText] = React.useState();
+  const [workList, setWorkList] = React.useState([]);
+
+  function setThisWork(text) {
+    setText(text);
+  }
+
+  function addItem(){
+    if (text !== ''){
+      let newList = [...workList, text]
+      setWorkList(newList)
+      setText('');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Primera Aplicación</Text>
-      <Text>Mi primera aplicación en React Native!!</Text>
-      <StatusBar style="auto" />
-      <Text style={styles.title}>¡GRACIAS!</Text>
+      <View style={styles.inputs}>
+        <Text style={styles.title}>To Do List</Text>
+        <Text style={styles.description}>Lista de actividades a realizar</Text>
+        {/* <StatusBar style="auto" /> */}
+        <TextInput
+          style={styles.textInput}
+          placeholder="Ingrese Tarea"
+          onChangeText={(text) => setThisWork(text)}
+          value={text}/>
+        <Button title="Agregar"
+          onPress={() => addItem()} />
+      </View>
+      <View style={styles.listContainer}>
+        {
+          workList.map((item) => {
+            return(
+              <Text style={styles.listItem} key={Math.random()*1000}>
+                {item}
+              </Text>
+            )
+          })
+        }
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 25
-  }
-});
---
+++ Desafio2/assets/styles/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listContainer: {
+    fontStyle: 'bold'
+  },
+  inputs: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  description: {
+    marginTop: 10
+  },
+  listItem: {
+    marginTop: 10,
+    marginBottom: 10,
+    fontStyle: 'italic'
+  },
+  title: {
+    fontSize: 25
+  },
+  textInput: {
+    borderWidth: 1,
+    width: 100,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingLeft: 5
+  }
+});
