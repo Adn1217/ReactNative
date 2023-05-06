c4bdffc8948679cb65e14c06d6d2e0373ba24cc5
Adrian Fernandez <77861592+Adn1217@users.noreply.github.com>
2023-05-06 17:24:02 -0500

    Se ajustan estilos generales

--- Desafio2/assets/styles/styles.js
+++ Desafio2/assets/styles/styles.js
@@ -23,23 +23,4 @@ export const styles = StyleSheet.create({
   },
   deleteButton: {
   },
-  modal: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    padding: 50,
-    backgroundColor: '#E0E0E0',
-    opacity: 0.9
-  },
-  modalDescription: {
-    fontWeight: 'bold'
-  },
-  modalButtons: {
-    flexDirection: 'row',
-    gap: 10,
-    marginTop: 10,
-    marginBottom: 10,
-    // alignItems: 'center',
-    justifyContent: 'space-between',
-  }
 });
